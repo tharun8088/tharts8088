27a449a151c76296cac9508e0dcef1ada142b4df
tharun8088 <tharuntharu8088@gmail.com>
2024-02-12 12:08:34 +0530

    added sort

--- abc.java
+++ abc.java
@@ -1,7 +1,11 @@
 class abc
 {
+    void sort()
+    {
+        return sorted array
+    }
     public static void main(String [] args)
     {
-        System.out.println("hlo")
+        System.out.println("hlo");
     } 
 }
